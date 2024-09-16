5df6d8a40f1c671eacb4c7f1235d2f92602c223a
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2024-09-16 17:23:38 -0300

    update

---
+++ Funções para Manipulação de strings/mod2_mp1.py
@@ -0,0 +1,12 @@
+arquivo = open('dados.txt', 'r', encoding='utf-8')
+
+conteudo = arquivo.read()
+
+print("Tipo de conteúdo: ", type(conteudo))
+
+
+#print ('Conteúdo retorado pelo read: ')
+
+#print(repr(conteudo))
+
+#arquivo.close()

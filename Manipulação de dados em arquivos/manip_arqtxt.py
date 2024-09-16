65b55821813781e773cacdbfe2181086a9b85d5b
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2024-09-16 16:04:43 -0300

    Update manip_arqtxt.py

--- Manipulação de dados em arquivos/manip_arqtxt.py
+++ Manipulação de dados em arquivos/manip_arqtxt.py
@@ -1,5 +1,17 @@
 #Manipulando arquivo-texto em Python
 
+#Situação-problema: Você foi contratado por uma editora para desenvolver um programa que ajude na revisão de textos. A editora recebe frequentemente textos de autores que contêm inconsistências no uso de maiúsculas e minúsculas, o que dificulta o processo de edição e publicação.
+
+#passos esperados no programa:
+
+#Captura de dados: o programa deve solicitar ao usuário que insira várias frases ou parágrafos pelo console. A entrada de dados deve continuar até o usuário digitar "sair", indicando que deseja encerrar a inserção e salvar o arquivo.
+
+#Criação e armazenamento: o texto inserido deve ser salvo em um arquivo chamado meu_arquivo.txt.
+
+#Manipulação do arquivo: o programa deve abrir o arquivo salvo, ler o conteúdo e converter todas as letras para maiúsculas.
+
+#Sobrescrever o arquivo original: após a manipulação, o texto alterado deve ser usado para sobrescrever o arquivo original, garantindo que o arquivo contenha apenas o texto formatado.
+
 def main():
     print("Digite suas frases. Digite 'sair' para terminar e salvar o arquivo.")
     frases = []

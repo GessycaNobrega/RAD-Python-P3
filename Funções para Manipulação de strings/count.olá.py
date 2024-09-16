8913ccf1b7a54094a483a7430d8e7d35ba9bd4e4
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2024-09-16 18:55:14 -0300

    Update count.olá,py

--- Funções para Manipulação de strings/count.olá.py
+++ Funções para Manipulação de strings/count.olá.py
@@ -1,3 +1,4 @@
+#sintaxe do count : contagem = variavel_string.count(palavra)
 with open ('dados.txt','r', encoding='utf-8') as arquivo:
     texto = arquivo.read()
     contador = texto.count("Olá")

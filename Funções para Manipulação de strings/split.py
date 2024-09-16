15fb327966e45e95c884793e45d144187f34d396
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2024-09-16 18:12:28 -0300

    Create split.py

---
+++ Funções para Manipulação de strings/split.py
@@ -0,0 +1,13 @@
+frase1 = "Eu amo comer amoras no café da manhã"
+
+lista_termos1 = frase1.split() #retorna uma lista separada pelos espaços
+print(lista_termos1)
+
+frase2 = "Amora Abacaxi   Abacate    Banana"
+lista_termos2 = frase2.split()
+print(lista_termos2)
+
+
+frase3 = "Amora Abacaxi   Abacate    Banana"
+lista_termos3 = frase2.split(',') #informando ao split que o sep é a ","
+print(lista_termos3)

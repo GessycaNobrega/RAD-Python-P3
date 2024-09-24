b9c860106d8b7bc4f140cf7dda31d334351d5e2d
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2024-09-24 18:02:58 -0300

    Createtable_sqlite.py

--- Python com Banco de dados/Módulo1/Módulo 2/Createtable_sqlite.py
+++ Python com Banco de dados/Módulo1/Módulo 2/Createtable_sqlite.py
@@ -9,3 +9,32 @@
 #Se quisermos criar um banco de dados em memória, que será criado para toda execução do programa, basta utilizar o comando conexao = sqlite3.connect(':memory:').
 
 import sqlite3 as conector
+
+try:
+    #Abertura de conexão e aquisição de cursor
+    conexao = conector.connect("./meu_banco.db")
+    cursor = conexao.cursor()
+
+    #Execução de um comando... CREAT, SELECT 
+    comando = '''CREATE TABLE Pessoa (
+                cpf INTEGER NOT NULL,
+                nome TEXT NOT NULL,
+                nascimento DATE NOT NULL,
+                oculos BOOLEAN NOT NULL,
+                PRIMARY KEY (cpf)
+                );'''
+    
+    cursor.execute(comando)
+
+    #Efetivação do comando
+    conexao.commit()
+
+except conector.DatabaseError as err:
+    print("Erro de banco de dados", err)
+
+finally:
+    # Fechamento das conexões
+    if conexao:
+        cursor.close(
+            conexao.close()
+        )

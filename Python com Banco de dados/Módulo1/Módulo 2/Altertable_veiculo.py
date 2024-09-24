be0275f4c3760660b8eaeff963f50bdd8170fe4d
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2024-09-24 19:05:37 -0300

    ltertable_veiculo.py

--- Python com Banco de dados/Módulo1/Módulo 2/Altertable_veiculo.py
+++ Python com Banco de dados/Módulo1/Módulo 2/Altertable_veiculo.py
@@ -1,21 +1,22 @@
 import sqlite3 as conector
 
 try:
-    #Abertura de conexão e aquisição de cursor
-    conexao = conector.connect("./meu_banco.db")
-    cursor = conexao.cursor()
+    # Abertura de conexão e aquisição de cursor
+    with conector.connect("./meu_banco.db") as conexao:
+        cursor = conexao.cursor()
 
-    #Execução de um comando... CREAT, SELECT 
-    comando = '''ALTER TABLE Veiculo
-                    ADD motor Real;'''
-    
-    cursor.execute(comando)
+        # Execução de um comando para adicionar coluna
+        comando = '''ALTER TABLE Veiculo
+                     ADD COLUMN motor REAL;'''
+        
+        cursor.execute(comando)
 
-    #Efetivação do comando
-    conexao.commit()
+        # Efetivação do comando
+        conexao.commit()
 
 except conector.DatabaseError as err:
-    print("Erro de banco de dados", err)
+    print("Erro de banco de dados:", err)
+
 
 finally:
     # Fechamento das conexões
--- Python com Banco de dados/Módulo1/Módulo 2/Createtable_veiculo.py
+++ Python com Banco de dados/Módulo1/Módulo 2/Createtable_veiculo.py
@@ -31,3 +31,17 @@ finally:
         cursor.close()
         conexao.close()
         
+
+#Para remover a tabela Veiculo, utilizamos o seguinte comando SQL : DROP TABLE Veiculo;
+
+#Para recriar tabelas na ordem desejada: CREATE TABLE Veiculo (
+#placa CHARACTER(7) NOT NULL,
+#ano INTEGER NOT NULL,
+#cor TEXT NOT NULL,
+#motor REAL NOT NULL,
+#proprietario INTEGER NOT NULL,
+#marca INTEGER NOT NULL,
+#PRIMARY KEY (placa),
+#FOREIGN KEY(proprietario) REFERENCES Pessoa(cpf),
+#FOREIGN KEY(marca) REFERENCES Marca(id)
+#);

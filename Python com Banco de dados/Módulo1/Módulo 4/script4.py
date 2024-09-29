62025ffcb598c1c8e7cc886514f002aea08cad9f
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2024-09-29 14:48:23 -0300

    Create
    Recuperação de registros em tabela.txt

---
+++ Python com Banco de dados/Módulo1/Módulo 4/script4.py
@@ -0,0 +1,23 @@
+import sqlite3 as conector
+from modelo import Veiculo
+
+#Abertura de conexão e aquisição de cursor 
+conexao=conector.connect("meu_banco.db")
+cursor = conexao.cursor()
+
+#Definicão dos comandos
+
+comando = ''' SELECT * FROM Veiculo; '''
+cursor.execute(comando)
+
+#Recuperação dos registros 
+
+reg_veiculos = cursor.fetchall()
+for reg_veiculo in reg_veiculos:
+    veiculo = Veiculo(*reg_veiculo)
+    print("Placa:", veiculo.placa, "Marca:", veiculo.marca)
+
+#fechamento das conexões
+
+cursor.close()
+conexao.close()

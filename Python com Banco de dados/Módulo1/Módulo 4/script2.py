2040137bdc62d8386e982caba548c9d678af5b52
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2024-09-29 13:05:12 -0300

    Create script1.py, script2.py, script3.py

---
+++ Python com Banco de dados/Módulo1/Módulo 4/script2.py
@@ -0,0 +1,21 @@
+#Vamos criar agora uma consulta para retornar às pessoas que usam óculos. 
+
+conexao = conector.connect("./meu_banco.db")
+cursor = conexao.cursor()
+
+ # Definição dos comandos
+comando = '''SELECT * FROM Pessoa WHERE oculos=:usa_oculos;'''
+cursor.execute(comando, {"usa_oculos": True})
+
+ # Recuperação dos registros
+registros = cursor.fetchall()
+for registro in registros:
+      pessoa = Pessoa(*registro)
+      print("cpf:", type(pessoa.cpf), pessoa.cpf)
+      print("nome:", type(pessoa.nome), pessoa.nome)
+      print("nascimento:", type(pessoa.data_nascimento), pessoa.data_nascimento)
+      print("oculos:", type(pessoa.usa_oculos), pessoa.usa_oculos)
+
+ # Fechamento das conexões
+cursor.close()
+conexao.close()

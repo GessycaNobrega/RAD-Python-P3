eb277fbec4c054a19e433cdc2dc0fdd6528712ce
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2024-09-30 19:01:32 -0300

    Update script4.py

--- Python com Banco de dados/Módulo1/Módulo 4/script4.py
+++ Python com Banco de dados/Módulo1/Módulo 4/script4.py
@@ -1,27 +1,42 @@
+#vamos buscar os veículos e suas respectivas marcas, utilizando junção de tabelas.
+
+
 import sqlite3 as conector
 from modelo import Veiculo
 
-#Abertura de conexão e aquisição de cursor 
-conexao=conector.connect("meu_banco.db")
+ # Abertura de conexão e aquisição de cursor
+conexao = conector.connect("./meu_banco.db")
 cursor = conexao.cursor()
 
-#Definicão dos comandos
-
-comando = ''' SELECT * FROM Veiculo; '''
+ # Definição dos comandos
+comando = '''SELECT * FROM Veiculo;'''
 cursor.execute(comando)
 
-#Recuperação dos registros 
-
+ # Recuperação dos registros
 reg_veiculos = cursor.fetchall()
 for reg_veiculo in reg_veiculos:
     veiculo = Veiculo(*reg_veiculo)
-    print("Placa:", veiculo.placa, ",Marca:", veiculo.marca)
+    print("Placa:", veiculo.placa, ", Marca:", veiculo.marca)
 
-#fechamento das conexões
-
+ # Fechamento das conexões
 cursor.close()
 conexao.close()
 
-#Esse script está correto, porém, a saída dele irá trazer na marca o resulto: "Marca 1" e "Marca 2 " e esse número 1 e 2 não se refere ao nome ou sigla da marca, e sim ao ID dessas marcar. O nome mesmo é MARCA A e MARCA B.
 
-#No script 5 será verificado como mostrar o nome da marca
+#Resultado da saída:
+# D:Banco de dados> & C:python.exe "d:/Banco de dados/script19_1.py"
+# Placa: AAA0001 , Marca: 1
+# Placa: BAA0002 , Marca: 1
+# Placa: CAA0003 , Marca: 2
+# Placa: DAA0004 , Marca: 2
+
+
+#Verifique no console que os valores do atributo marca são seus respectivos ids, 1 e 2. Isso ocorre porque no banco de dados armazenamos apenas uma referência à chave primária da entidade Marca.
+
+
+
+#E se quisermos substituir o id das marcas pelos seus respectivos nomes? Para isso, precisamos realizar uma junção das tabelas Veículo e Marca no comando SELECT.
+
+
+#O comando SELECT para junção de duas tabelas tem a seguinte sintaxe:
+#SELECT tab1.col1, tab1.col2, tab2.col1… FROM tab1 JOIN tab2 ON tab1.colN = tab2.colM;
--- Python com Banco de dados/Módulo1/Módulo 4/script5.py
+++ Python com Banco de dados/Módulo1/Módulo 4/script5.py
@@ -1,27 +1,35 @@
-#A solução do problema no script 4 será a junção, ou seja , o JOIN 
+#Verifique no console, do resultado do script4, que os valores do atributo marca são seus respectivos ids, 1 e 2. Isso ocorre porque no banco de dados armazenamos apenas uma referência à chave primária da entidade Marca.
+
+#E se quisermos substituir o id das marcas pelos seus respectivos nomes?
+
+# Resposta: Para isso, precisamos realizar uma junção das tabelas Veículo e Marca no comando SELECT. O comando SELECT para junção de duas tabelas tem a seguinte sintaxe: SELECT tab1.col1, tab1.col2, tab2.col1… FROM tab1 JOIN tab2 ON tab1.colN = tab2.colM;
+
+
+#Primeiro, definimos quais colunas serão retornadas utilizando a sintaxe nome_tabela.nome_coluna, depois indicamos as tabelas que desejamos juntar e, por último, indicamos como alinhar os registros de cada tabela, ou seja, quais são os atributos que devem ser iguais (colN e colM).
+
+
+#No exemplo a seguir, vamos criar um script de forma que o Veículo tenha acesso ao nome da Marca, não ao id.
 
 import sqlite3 as conector
 from modelo import Veiculo
 
-#Abertura de conexão e aquisição de cursor 
-conexao=conector.connect("meu_banco.db")
+ # Abertura de conexão e aquisição de cursor
+conexao = conector.connect("./meu_banco.db")
 cursor = conexao.cursor()
 
-comando = '''SELECT 
-                Veiculo.placa, Veiculo.ano, Veiculo.cor,
-                Veiculo.motor, Veiculo.proprietario,
-                Marca.nome FROM Veiculo JOIN ON Marca.id = Veiculo.marca;'''
+ # Definição dos comandos 
+comando = ''' 
+ SELECT Veiculo.placa, Veiculo.ano, Veiculo.cor, Veiculo.motor, Veiculo.proprietario, Marca.nome 
+ FROM Veiculo 
+ JOIN Marca ON Marca.id = Veiculo.marca;'''
 cursor.execute(comando)
 
-
-#Recuperação dos registros 
-
+ # Recuperação dos registros
 reg_veiculos = cursor.fetchall()
 for reg_veiculo in reg_veiculos:
     veiculo = Veiculo(*reg_veiculo)
-    print("Placa:", veiculo.placa, ",Marca:", veiculo.marca)
+    print("Placa:", veiculo.placa, ", Marca:", veiculo.marca)
 
-#fechamento das conexões
-
+ # Fechamento das conexões
 cursor.close()
 conexao.close()

2bcbccd2bc695178380cb64bb62c5e79544ad11d
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2024-10-01 17:18:30 -0300

    Create tkinter.py

---
+++ Interface gráfica com Python/Módulo 1/tkinter.py
@@ -0,0 +1,15 @@
+#É o framework GUI padrão do python. Sua sintaxe é simples, possui muitos componentes para interação com o usuário. Além disso, seu código é aberto e é disponível sob a licença python
+
+#Caso ela não esteja instalada na sua versão do python, basta digitar o comando:
+
+pip install tkinter
+
+#Para quem usa a IDE Spyder (SPYDER, 2020), é necessário colocar uma exclamação antes do comando “pip”, ou seja:
+
+!pip install tkinter
+
+
+#Para testar a instalação, basta escrever o seguinte código na linha de comando:
+
+import tkinter
+tkinter._test()

509d88b10625a0c2e2603196ae127e1814e15306
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2024-10-01 17:21:09 -0300

    Update flexx.py

--- Interface gráfica com Python/Módulo 1/flexx.py
+++ Interface gráfica com Python/Módulo 1/flexx.py
@@ -1,3 +1,16 @@
+#É um kit de ferramentas para o desenvolvimento de interfaces gráficas com o usuário implementado em python que faz uso de tecnologia web para sua renderização. O Flexx pode ser usado para criar tanto aplicações de desktop como para web e até mesmo exportar uma aplicação para um documento HTML independente.
+
+
+#Para instalar o Flexx, basta digitar o comando:
+
+pip install flexx
+
+#No caso do Spyder, o comando deve ser:
+
+!pip install flexx
+
+#Uma forma prática de testar a instalação e aprender um pouco mais sobre esse framework é escrever o código abaixo na linha de comando, ou em um arquivo, e executar:
+
 from flexx import flx
 class Exemplo(flx.Widget):
 
@@ -9,4 +22,3 @@ if __name__ == '__main__':
     a = flx.App(Exemplo, title='Flexx demonstração')
     m = a.launch()
     flx.run()
-    

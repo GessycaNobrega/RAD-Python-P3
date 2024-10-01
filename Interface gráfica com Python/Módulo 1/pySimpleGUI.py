77fdd58ae71d1d1d8187e03cb97b5bac560c0ed1
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2024-10-01 17:54:22 -0300

    Create pySimpleGUI.py

---
+++ Interface gráfica com Python/Módulo 1/pySimpleGUI.py
@@ -0,0 +1,10 @@
+#PySimpleGUI
+
+#Esse pacote foi lançado em 2018 e possui portabilidade com os pacotes: Tkinter, PyQt, wxpython e Remi, portanto aumenta as possibilidades de uso de componentes na programação
+
+#Para fazer a instalação básica, é necessário escrever na linha de comando:
+
+pip install pysimplegui
+
+
+#Uma forma de testar a instalação é escrever e executar o programa:
